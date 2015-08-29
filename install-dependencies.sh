4757eeaaa1f8bb626299ec0fc641db387d1ca905
B. van Berkum <dev@dotmpe.com>
2015-08-29 20:43:32 +0200

    Install deps. scritp verbosity (II)

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -8,18 +8,21 @@ source ./util.sh
 install_jjb()
 {
   log "Cloning JJB.."
-  git clone https://git.openstack.org/openstack-infra/jenkins-job-builder $HOME/build/jjb
+  git clone https://git.openstack.org/openstack-infra/jenkins-job-builder $HOME/build/jjb \
+    || err "Error cloning to $HOME/build/jjb" 1
 
   log "Installing JJB.."
   pushd $HOME/build/jjb
   #sudo python setup.py install
-  python setup.py install --user
+  python setup.py install --user \
+    && log "JJB install complete" \
+    || err "Error during JJB installation" 1
   popd
 
-
-  log "JJB install complete. Testing.."
-  jenkins-jobs --version
-
-  log "JJB install OK"
+  jenkins-jobs --version && {
+    log "JJB install OK"
+  } || {
+    err "JJB installation invalid" 1
+  }
 }
 
