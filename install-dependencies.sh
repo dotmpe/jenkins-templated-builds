004e441fbb293edb5290ffd333ae71640cebff79
B. van Berkum <dev@dotmpe.com>
2015-08-29 20:40:05 +0200

    Install deps. scritp verbosity

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,16 +3,23 @@
 # JTB dep installer for travis
 # Id: jtb install-dependencies.sh
 
+source ./util.sh
 
 install_jjb()
 {
+  log "Cloning JJB.."
   git clone https://git.openstack.org/openstack-infra/jenkins-job-builder $HOME/build/jjb
 
+  log "Installing JJB.."
   pushd $HOME/build/jjb
   #sudo python setup.py install
   python setup.py install --user
   popd
 
+
+  log "JJB install complete. Testing.."
   jenkins-jobs --version
+
+  log "JJB install OK"
 }
 
