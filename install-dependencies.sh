93d8fb2a3fba410e1d2e6de76f3da26a124d2a1f
B. van Berkum <dev@dotmpe.com>
2015-08-31 17:00:16 +0200

    -

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -5,32 +5,32 @@
 
 . ./util.sh
 
-test -n "$JJB_HOME" || {
-    # default checkout dir at travis
-    JJB_HOME=$HOME/build/jjb
-}
+if test -n "$JJB_HOME"
+then
+  # default checkout dir at travis
+  JJB_HOME=$HOME/build/jjb
+fi
 
 install_jjb()
 {
-
   test -d "$JJB_HOME" && {
 
     log "JJB_HOME exists: $JJB_HOME"
   } || {
 
-      log "Cloning JJB.."
-      git clone https://git.openstack.org/openstack-infra/jenkins-job-builder $JJB_HOME \
-        || err "Error cloning to $JJB_HOME" 1
+    log "Cloning JJB.."
+    git clone https://git.openstack.org/openstack-infra/jenkins-job-builder $JJB_HOME \
+      || err "Error cloning to $JJB_HOME" 1
 
-      log "Installing JJB.."
-      pushd $JJB_HOME
+    log "Installing JJB.."
+    pushd $JJB_HOME
 
-      # Install into ~/..user-packages
-      python setup.py install --user \
-        && log "JJB install complete" \
-        || err "Error during JJB installation" 1
+    # Install into ~/..user-packages
+    python setup.py install --user \
+      && log "JJB install complete" \
+      || err "Error during JJB installation" 1
 
-      popd
+    popd
   }
 
   jenkins-jobs --version && {
