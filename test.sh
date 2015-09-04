44e9bc54ec9ff678f4b8a6520bdc036ed1189156
B. van Berkum <dev@dotmpe.com>
2015-09-04 03:25:37 +0200

    Cleaning. Some minor fixes.

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-test -n "$files" || files=tpl/base.yaml:jtb.yaml
-
-jenkins-jobs test $files
-
-# Id: jtb/0.0.2-test test.sh
--- util.sh
+++ util.sh
@@ -3,14 +3,31 @@
 # stdio/stderr/exit util
 log()
 {
-	[ -n "$(echo "$*")" ] || return 1;
-	echo "[$scriptname.sh] $1"
+  [ -n "$(echo "$*")" ] || return 1;
+  echo "[$scriptname.sh] $1"
 }
 err()
 {
-	[ -n "$(echo "$*")" ] || return 1;
-	echo "$1 [$scriptname.sh]" 1>&2
-	[ -n "$2" ] && exit $2
+  case "$(echo $1 | tr 'A-Z' 'a-z')" in
+    warn*|err*|notice ) log "$1: $2" 1>&2 ;;
+    * ) log "$2" 1>&2 ;;
+  esac
+  [ -z $3 ] || exit $3
 }
-
+error()
+{
+  err "Error" "$1" "$2"
+}
+warn()
+{
+  err "Warning" "$1" "$2"
+}
+note()
+{
+  err "Notice" "$1" "$2"
+}
+info()
+{
+  err " " "$1" "$2"
+}
 
